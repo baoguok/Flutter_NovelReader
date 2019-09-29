376d9343723fbaa25aeeec90ef66153235909bef
“tongyangsheng” <964997115@qq.com>
2019-09-29 17:40:48 +0800

    新增福利页

---
+++ lib/pages/gift/gift_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class GiftPage extends StatefulWidget {
+  GiftPage({Key key}) : super(key: key);
+
+  @override
+  _GiftPageState createState() {
+    return _GiftPageState();
+  }
+}
+
+class _GiftPageState extends State<GiftPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      child: Center(
+        child: Text(
+          '福利'
+        ),
+      ),
+    );
+  }
+}
