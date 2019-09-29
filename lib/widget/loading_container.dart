a19bae1ca5bc7047cff77b76069cb6baeec32c92
“tongyangsheng” <964997115@qq.com>
2019-09-29 17:43:05 +0800

    新增加载界面组件

---
+++ lib/widget/loading_container.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class LoadingContainer extends StatelessWidget {
+  final Widget child;
+  final bool isLoading;
+  final bool cover;
+
+  const LoadingContainer({Key key, @required this.isLoading, this.cover = false,
+    @required this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return !cover ? !isLoading ? child : _loadingView:
+    Stack(
+      children: <Widget>[
+        child, isLoading?_loadingView:null
+      ],
+    );
+  }
+
+  Widget get _loadingView {
+    return Center
+      (
+      child: CircularProgressIndicator
+        (
+
+      ),
+    );
+  }
+}
+
