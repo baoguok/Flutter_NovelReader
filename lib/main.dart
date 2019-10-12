569e7c0209f6865a97b9d742f198f55157fce2ab
“tongyangsheng” <964997115@qq.com>
2019-10-12 17:22:58 +0800

    改动删除

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_reader/navigator/tab_navigator.dart';
 
-void main() => runApp(MyApp());
-
+void main() {
+  runApp(MyApp());
+}
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
