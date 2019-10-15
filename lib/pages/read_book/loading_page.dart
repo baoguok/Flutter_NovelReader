a1ef914f1336ecc846831d8c1d5727db00fc91fd
“tongyangsheng” <964997115@qq.com>
2019-10-15 17:30:10 +0800

    书籍加载页

---
+++ lib/pages/read_book/loading_page.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class LoadPage extends StatefulWidget {
+  LoadPage({Key key}) : super(key: key);
+
+  @override
+  _LoadPageState createState() {
+    return _LoadPageState();
+  }
+}
+
+class _LoadPageState extends State<LoadPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
+
+
+class FailureView extends StatefulWidget {
+  final OnLoadReloadListener _listener;
+
+  FailureView(this._listener);
+
+  @override
+  State<StatefulWidget> createState() => _FailureViewState();
+}
+
+class _FailureViewState extends State<FailureView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.redAccent,
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Image.asset(
+            "images/icon_network_error.png",
+            width: 150,
+            height: 150,
+          ),
+          SizedBox(
+            height: 14,
+          ),
+          Text(
+            "咦？没网络啦~检查下设置吧",
+            style: TextStyle(fontSize: 12, color: Colors.redAccent),
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          MaterialButton(
+            onPressed: () {
+              this.widget._listener.onReload();
+            },
+            minWidth: 150,
+            height: 43,
+            color: Colors.redAccent,
+            child: Text(
+              "重新加载",
+              style: TextStyle(
+                color: Colors.redAccent,
+                fontSize: 16,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+abstract class OnLoadReloadListener {
+  void onReload();
+}
+
+
+//加载状态
+enum LoadStatus {
+  LOADING,
+  SUCCESS,
+  FAILURE,
+}
