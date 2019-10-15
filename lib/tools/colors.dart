edfc0ec74afecf56f5041b1c40cb52225a5fcc5c
“tongyangsheng” <964997115@qq.com>
2019-10-15 17:31:12 +0800

    全局颜色文件

---
+++ lib/tools/colors.dart
@@ -0,0 +1,40 @@
+
+import 'package:flutter/material.dart';
+
+class MyColors{
+  static final Color white = Color(0xFFFFFFFF);
+  static final Color black = Color(0xFF000000);
+
+  static final Color contentColor = Color(0xFFAAAAAA);
+  static final Color contentBgColor = Color(0xFA303030);
+
+
+  static final Color primary = Color(0xFFFFFFFF);
+  static final Color textPrimaryColor = Color(0xFF33C3A5);
+
+  static final Color homeTabText = Color(0xFF33C3A5);
+  static final Color homeTabGreyText = Color(0xFF606060);
+  static final Color homeGrey = Color(0xFFF5F5F5);
+  static final Color homeGreyText = Color(0xFFCCCCCC);
+
+  static final Color textBlack3 = Color(0xFF31323E);
+  static final Color textBlack6 = Color(0xFF6A6C7A);
+  static final Color textBlack9 = Color(0xFF9A9AA7);
+
+  static final Color infoBgColor = Color(0xFF605167);
+  static final Color fractionColor = Color(0xFFFF7634);
+
+  static final Color dividerColor = Color(0xFFF6F7F8);
+  static final Color dividerDarkColor = Color(0xFFEBEDF0);
+
+  static final Color titleTextColor = Color(0xFF333333);
+  static final Color meBgColor = Color(0xFF545A65);
+  static final Color meTextColor = Color(0xFF85682F);
+  static final Color meRedTextColor = Color(0xFFFF7634);
+  static final Color paginationColor = Color(0xFFD8D8D8);
+
+
+
+
+
+}
