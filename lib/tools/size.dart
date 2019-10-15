90f87b6dc806acdfa33f6cebff09a540b50408b7
“tongyangsheng” <964997115@qq.com>
2019-10-15 17:31:31 +0800

    全局布局大小文件

---
+++ lib/tools/size.dart
@@ -0,0 +1,12 @@
+
+class MySize {
+  static final double leftMargin = 15;
+  static final double rightMargin = 15;
+  static final double titleTextSize = 18;
+  static final double textSizeM = 16;
+  static final double textSizeL = 14;
+  static double homeImageSize = 25;
+
+  static double titleHeight = 48;
+
+}
