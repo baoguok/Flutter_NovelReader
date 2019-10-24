22dcc72bd59796835a1d54a7a1a10cbf97c35046
“tongyangsheng” <964997115@qq.com>
2019-10-24 17:41:21 +0800

    全局登录状态控制

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_reader/navigator/tab_navigator.dart';
+import 'package:flutter_reader/tools/global_state.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp(
-  ));
+  final hasLogin = GlobalModel();
+
+  runApp(
+    Provider<bool>.value(
+    child: ChangeNotifierProvider.value(value: hasLogin,
+    child: MyApp(),),
+    )
+  );
 }
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
