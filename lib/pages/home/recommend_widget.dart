ae3f6175cb3f9a53f4fdfce4f710ca84ccec7b9a
“tongyangsheng” <964997115@qq.com>
2019-10-29 16:05:35 +0800

    主编推荐 分区数据加载

--- lib/pages/home/recommend_widget.dart
+++ lib/pages/home/recommend_widget.dart
@@ -1,27 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class RecommendWidget extends StatelessWidget {
-  RecommendWidget({Key key}) : super(key: key);
+
+class RecommendWidget extends StatefulWidget {
+
+  final List<String> _imageList;
+  final List<String> _titleList;
+
+  RecommendWidget(this._imageList, this._titleList);
+
+
+  @override
+  _RecommendWidgetState createState() {
+    return _RecommendWidgetState();
+  }
+}
+
+class _RecommendWidgetState extends State<RecommendWidget> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Container(
       decoration: BoxDecoration(
-        color: Colors.white
+          color: Colors.white
       ),
       height: ScreenUtil().setHeight(600),
       child: Row(
         children: <Widget>[
-             _subItem('bookImage/book4.jpg', '欢爱一生'),
-             _subItem('bookImage/book5.jpg', '星途璀璨'),
-             _subItem('bookImage/book6.jpg', '一世倾情'),
+          _subItem(widget._imageList[0], widget._titleList[0]),
+          _subItem(widget._imageList[1], widget._titleList[1]),
+          _subItem(widget._imageList[2], widget._titleList[2]),
         ],
       ),
     );
   }
-
   _subItem(String image, String title){
     return Container(
       margin: EdgeInsets.only(left: ScreenUtil().setWidth(50)),
@@ -45,15 +67,15 @@ class RecommendWidget extends StatelessWidget {
             child: Image(
               width: ScreenUtil().setWidth(310),
               height: ScreenUtil().setHeight(400),
-              image: AssetImage(image),
+              image: NetworkImage(image),
               fit: BoxFit.fill,
             ),
           ),
           Container(
-            margin: EdgeInsets.only(top: ScreenUtil().setHeight(35)),
-          child:Text(
-            title
-          )
+              margin: EdgeInsets.only(top: ScreenUtil().setHeight(35)),
+              child:Text(
+                  title
+              )
           )
         ],
       ),
