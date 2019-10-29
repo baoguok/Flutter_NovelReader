0811477179e37c7b0d45a5682e0b9e01cc93f37c
“tongyangsheng” <964997115@qq.com>
2019-10-29 16:06:36 +0800

    新书抢先 分区数据加载

--- lib/pages/home/newbook_widget.dart
+++ lib/pages/home/newbook_widget.dart
@@ -2,56 +2,79 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class NewBookWidget extends StatelessWidget {
-  NewBookWidget({Key key}) : super(key: key);
+class NewBookWidget extends StatefulWidget {
+  final List<String> _newName;
+  final List<String> _newImage;
+  final List<String> _newCat;
+  final List<String> _newDesc;
+  final List<String> _newStatus;
+  final List<int> _newClicks;
+
+  NewBookWidget(this._newName, this._newImage, this._newCat, this._newDesc, this._newStatus, this._newClicks);
+
+  @override
+  _NewBookWidgetState createState() {
+    return _NewBookWidgetState();
+  }
+}
+
+class _NewBookWidgetState extends State<NewBookWidget> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Container(
-      decoration: BoxDecoration(
-        color: Colors.white
-      ),
-      height: ScreenUtil().setHeight(1250),
-      child: Container(
-        child: Column(
-          children: <Widget>[
-            _getMainItem('bookImage/book7.jpg', '情花开','[连载中]:', '郎骑竹马来，绕床弄青梅故事里面没有青梅，只有失散七年的竹马与竹马。再次相遇，可就不能让缘分就这么悄无声息的溜走，一定要','都市娱乐',4785),
-            _getMainItem('bookImage/book8.jpg', '爱的彼方','[已完结]:', '你承诺要给我一个没有狂风暴雨的世界但我更希望站到你身和你一同担负起这个国家穿越时空','都市娱乐',7854),
-            _getMainItem('bookImage/book9.jpg', '无尽沉沦','[已完结]:', '地球的表面出现了九个巨坑，有人从其中挖出了一口石棺，里面躺着九具衣冠整洁的干尸。一棺九尸，开启了人类最黑暗的末日纪元。红颜薄命，英雄气短。','都市娱乐',5789)
-          ],
+        decoration: BoxDecoration(
+            color: Colors.white
+        ),
+        height: ScreenUtil().setHeight(1250),
+        child: Container(
+            child: Column(
+              children: <Widget>[
+                _getMainItem(widget._newImage[0], widget._newName[0],'[${widget._newStatus[0]}]:', widget._newDesc[0],widget._newCat[0],widget._newClicks[0]),
+                _getMainItem(widget._newImage[1], widget._newName[1],'[${widget._newStatus[1]}]:', widget._newDesc[1],widget._newCat[1],widget._newClicks[1]),
+                _getMainItem(widget._newImage[2], widget._newName[2],'[${widget._newStatus[2]}]:', widget._newDesc[2],widget._newCat[2],widget._newClicks[2])
+              ],
+            )
         )
-      )
     );
   }
-
   _getMainItem(String imageName, String title, String state, String introduce, String type, int readTimes){
     return Container(
       child: Row(
         children: <Widget>[
-         Container(
-      decoration: BoxDecoration(
-        boxShadow: <BoxShadow>[
-        new BoxShadow(
-        color: Colors.black26,
-        blurRadius: 5.0,
-        spreadRadius: 1.0,
-        offset: Offset(-2.0, 2.0),
-      ),
-      ],
-    ),
-           margin: EdgeInsets.fromLTRB(
-               ScreenUtil().setWidth(40),
-               ScreenUtil().setHeight(50),
-               ScreenUtil().setWidth(20),
-               ScreenUtil().setHeight(0)),
-           child: Image(
-             width: ScreenUtil().setWidth(280),
-             height: ScreenUtil().setHeight(350),
-             image: AssetImage(imageName),
-             fit: BoxFit.fill,
-           ),
-         ),
+          Container(
+            decoration: BoxDecoration(
+              boxShadow: <BoxShadow>[
+                new BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 5.0,
+                  spreadRadius: 1.0,
+                  offset: Offset(-2.0, 2.0),
+                ),
+              ],
+            ),
+            margin: EdgeInsets.fromLTRB(
+                ScreenUtil().setWidth(40),
+                ScreenUtil().setHeight(50),
+                ScreenUtil().setWidth(20),
+                ScreenUtil().setHeight(0)),
+            child: Image(
+              width: ScreenUtil().setWidth(280),
+              height: ScreenUtil().setHeight(350),
+              image: NetworkImage(imageName),
+              fit: BoxFit.fill,
+            ),
+          ),
           _rightItem(title, state, introduce, type, readTimes)
         ],
       ),
@@ -67,42 +90,42 @@ class NewBookWidget extends StatelessWidget {
             width: ScreenUtil().setWidth(700),
             height: ScreenUtil().setHeight(100),
             child:  Text(
-                title,
-            style: TextStyle(
-              fontSize: ScreenUtil().setSp(50),
-              fontWeight: FontWeight.w500
-            ),),
+              title,
+              style: TextStyle(
+                  fontSize: ScreenUtil().setSp(50),
+                  fontWeight: FontWeight.w500
+              ),),
           ),
           Container(
-            width: ScreenUtil().setWidth(700),
-            height: ScreenUtil().setHeight(120),
-            child: Row(
-              children: <Widget>[
-                Container(
-                  child: Align(
-                    alignment: Alignment.topLeft,
-                    child: Text(
-                      state,
-                      style: TextStyle(
-                          color: state == '[连载中]:' ? Colors.lightBlue : Colors.orangeAccent
+              width: ScreenUtil().setWidth(700),
+              height: ScreenUtil().setHeight(120),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        state,
+                        style: TextStyle(
+                            color: state == '[连载中]:' ? Colors.lightBlue : Colors.orangeAccent
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Container(
-                  width: ScreenUtil().setWidth(520),
-                  child: Text(
-                      introduce,
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 2,
-                      style: TextStyle(
-                          color: Colors.black54,
-                          fontSize: ScreenUtil().setSp(40)
-                      ),
-                )
-                )
-              ],
-            )
+                  Container(
+                      width: ScreenUtil().setWidth(520),
+                      child: Text(
+                        introduce,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
+                        style: TextStyle(
+                            color: Colors.black54,
+                            fontSize: ScreenUtil().setSp(40)
+                        ),
+                      )
+                  )
+                ],
+              )
           ),
           _bottom(type, readTimes)
         ],
@@ -112,49 +135,49 @@ class NewBookWidget extends StatelessWidget {
 
   _bottom(String type, int readTimes){
     return Container(
-      child: Row(
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.fromLTRB(0, ScreenUtil().setHeight(50), ScreenUtil().setWidth(320), 0),
-            width: ScreenUtil().setWidth(150),
-      decoration: BoxDecoration(
-          border: Border.all(
-          color: Colors.grey,
-          width: 1.0,
-          style: BorderStyle.solid
-      ),
-      borderRadius:BorderRadius.all(Radius.circular(5.0))
-      ),
-            child: Text(
-              type,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: ScreenUtil().setSp(30),
-                color: Colors.grey
+        child: Row(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.fromLTRB(0, ScreenUtil().setHeight(50), ScreenUtil().setWidth(320), 0),
+              width: ScreenUtil().setWidth(150),
+              decoration: BoxDecoration(
+                  border: Border.all(
+                      color: Colors.grey,
+                      width: 1.0,
+                      style: BorderStyle.solid
+                  ),
+                  borderRadius:BorderRadius.all(Radius.circular(5.0))
+              ),
+              child: Text(
+                type,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: ScreenUtil().setSp(30),
+                    color: Colors.grey
+                ),
               ),
             ),
-          ),
-          Container(
-            margin: EdgeInsets.fromLTRB(0, ScreenUtil().setHeight(60), 0, 0),
-            child: Image(
-              width: ScreenUtil().setWidth(50),
-              height: ScreenUtil().setHeight(30),
-              fit: BoxFit.fill,
-              image: AssetImage('images/浏览眼睛@2x.png'),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, ScreenUtil().setHeight(60), 0, 0),
+              child: Image(
+                width: ScreenUtil().setWidth(50),
+                height: ScreenUtil().setHeight(30),
+                fit: BoxFit.fill,
+                image: AssetImage('images/浏览眼睛@2x.png'),
+              ),
             ),
-          ),
-          Container(
-            margin: EdgeInsets.fromLTRB(ScreenUtil().setWidth(30), ScreenUtil().setHeight(60), 0, 0),
-            child: Text(
+            Container(
+              margin: EdgeInsets.fromLTRB(ScreenUtil().setWidth(30), ScreenUtil().setHeight(60), 0, 0),
+              child: Text(
                 readTimes.toString(),
-              style: TextStyle(
-                fontSize: ScreenUtil().setSp(35),
-                color: Colors.redAccent
+                style: TextStyle(
+                    fontSize: ScreenUtil().setSp(35),
+                    color: Colors.redAccent
+                ),
               ),
-            ),
-          )
-        ],
-      )
+            )
+          ],
+        )
     );
   }
 }
