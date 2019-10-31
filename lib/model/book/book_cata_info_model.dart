5b527d7fcf7be1565a41fcc5febe844a54663b77
“tongyangsheng” <964997115@qq.com>
2019-10-31 15:37:05 +0800

    书籍目录简介Model

---
+++ lib/model/book/book_cata_info_model.dart
@@ -0,0 +1,131 @@
+class CataInfoModel {
+  bool _success;
+  CataInfoData _data;
+
+  CataInfoModel({bool success, CataInfoData data}) {
+    this._success = success;
+    this._data = data;
+  }
+
+  bool get success => _success;
+  set success(bool success) => _success = success;
+  CataInfoData get data => _data;
+  set data(CataInfoData data) => _data = data;
+
+  CataInfoModel.fromJson(Map<String, dynamic> json) {
+    _success = json['success'];
+    _data = json['data'] != null ? new CataInfoData.fromJson(json['data']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['success'] = this._success;
+    if (this._data != null) {
+      data['data'] = this._data.toJson();
+    }
+    return data;
+  }
+}
+
+class CataInfoData {
+  First _first;
+  Last _last;
+  int _total;
+
+  CataInfoData({First first, Last last, int total}) {
+    this._first = first;
+    this._last = last;
+    this._total = total;
+  }
+
+  First get first => _first;
+  set first(First first) => _first = first;
+  Last get last => _last;
+  set last(Last last) => _last = last;
+  int get total => _total;
+  set total(int total) => _total = total;
+
+  CataInfoData.fromJson(Map<String, dynamic> json) {
+    _first = json['first'] != null ? new First.fromJson(json['first']) : null;
+    _last = json['last'] != null ? new Last.fromJson(json['last']) : null;
+    _total = json['total'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this._first != null) {
+      data['first'] = this._first.toJson();
+    }
+    if (this._last != null) {
+      data['last'] = this._last.toJson();
+    }
+    data['total'] = this._total;
+    return data;
+  }
+}
+
+class First {
+  String _id;
+  String _name;
+  String _logo;
+
+  First({String id, String name, String logo}) {
+    this._id = id;
+    this._name = name;
+    this._logo = logo;
+  }
+
+  String get id => _id;
+  set id(String id) => _id = id;
+  String get name => _name;
+  set name(String name) => _name = name;
+  String get logo => _logo;
+  set logo(String logo) => _logo = logo;
+
+  First.fromJson(Map<String, dynamic> json) {
+    _id = json['id'];
+    _name = json['name'];
+    _logo = json['logo'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this._id;
+    data['name'] = this._name;
+    data['logo'] = this._logo;
+    return data;
+  }
+}
+
+class Last {
+  String _id;
+  String _name;
+  String _logo;
+
+  Last({String id, String name, String logo}) {
+    this._id = id;
+    this._name = name;
+    this._logo = logo;
+  }
+
+  String get id => _id;
+  set id(String id) => _id = id;
+  String get name => _name;
+  set name(String name) => _name = name;
+  String get logo => _logo;
+  set logo(String logo) => _logo = logo;
+
+  Last.fromJson(Map<String, dynamic> json) {
+    _id = json['id'];
+    _name = json['name'];
+    _logo = json['logo'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this._id;
+    data['name'] = this._name;
+    data['logo'] = this._logo;
+    return data;
+  }
+}
