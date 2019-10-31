6744c12140fa2720274b7f2222c975e79acf2b1c
“tongyangsheng” <964997115@qq.com>
2019-10-31 15:40:32 +0800

    猜你喜欢 分区 动态加载数据

--- lib/pages/home/guess_widget.dart
+++ lib/pages/home/guess_widget.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_reader/pages/read_book/book_introduction.dart';
+import 'package:flutter_reader/widget/book_hero.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class GuessWidget extends StatefulWidget {
+  final String _channel;
 
+   final List<String> _bookId;
    final List<String> _imageList;
    final List<String> _titleList;
 
-   GuessWidget(this._imageList, this._titleList);
+   GuessWidget(this._channel,this._bookId,this._imageList, this._titleList);
 
 
   @override
@@ -40,16 +44,16 @@ class _GuessWidgetState extends State<GuessWidget> {
           children: <Widget>[
             Row(
               children: <Widget>[
-                _subItem(widget._imageList[0], widget._titleList[0]),
-                _subItem(widget._imageList[1], widget._titleList[1]),
-                _subItem(widget._imageList[2], widget._titleList[2])
+                _subItem(widget._bookId[0],widget._imageList[0], widget._titleList[0]),
+                _subItem(widget._bookId[1],widget._imageList[1], widget._titleList[1]),
+                _subItem(widget._bookId[2],widget._imageList[2], widget._titleList[2])
               ],
             ),
             Row(
               children: <Widget>[
-                _subItem(widget._imageList[3], widget._titleList[3]),
-                _subItem(widget._imageList[4], widget._titleList[4]),
-                _subItem(widget._imageList[5], widget._titleList[5])
+                _subItem(widget._bookId[3],widget._imageList[3], widget._titleList[3]),
+                _subItem(widget._bookId[4],widget._imageList[4], widget._titleList[4]),
+                _subItem(widget._bookId[5],widget._imageList[5], widget._titleList[5])
               ],
             )
           ],
@@ -58,33 +62,24 @@ class _GuessWidgetState extends State<GuessWidget> {
     );
   }
 
-  _subItem(String image, String title){
+  _subItem(String id, String image, String title){
     return Container(
       margin: EdgeInsets.only(left: ScreenUtil().setWidth(50)),
       width: ScreenUtil().setWidth(310),
       padding: EdgeInsets.only(top: ScreenUtil().setHeight(40)),
       child: Column(
         children: <Widget>[
-          Container(
-            height: ScreenUtil().setHeight(400),
-            margin: EdgeInsets.only(top: 7),
-            decoration: BoxDecoration(
-              boxShadow: <BoxShadow>[
-                new BoxShadow(
-                  color: Colors.black26,
-                  blurRadius: 5.0,
-                  spreadRadius: 1.0,
-                  offset: Offset(-2.0, 2.0),
-                ),
-              ],
-            ),
-            child: Image(
-              width: ScreenUtil().setWidth(310),
-              height: ScreenUtil().setHeight(400),
-              image: NetworkImage(image),
-              fit: BoxFit.fill,
-            ),
-          ),
+          BookHero(
+          book: image,
+          height: ScreenUtil().setHeight(400),
+          callback: (){
+            Navigator.of(context).push(MaterialPageRoute<void>(
+                builder: (BuildContext context){
+                  return BookInfoPage(channel:widget._channel,bookId: id,bookName: title,bookImage: image,isHorizontal: false,hasCollect: false,);
+                }
+            ));
+          }
+      ),
           Container(
               margin: EdgeInsets.only(top: ScreenUtil().setHeight(35)),
               child:Text(
