cc2310112626e9fa4f906a7d34ccfb93487b4916
“tongyangsheng” <964997115@qq.com>
2019-11-01 16:02:56 +0800

    Web环境新增

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/tongyangsheng/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/tongyangsheng/Desktop/Job/Flutter_workspace/ReaderProject/flutter_reader"
-export "FLUTTER_TARGET=/Users/tongyangsheng/Desktop/Job/Flutter_workspace/ReaderProject/flutter_reader/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/tongyangsheng/Desktop/Job/Flutter_workspace/ReaderProject/flutter_reader_web"
+export "FLUTTER_TARGET=/Users/tongyangsheng/Desktop/Job/Flutter_workspace/ReaderProject/flutter_reader_web/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/tongyangsheng/development/flutter/bin/cache/artifacts/engine/ios"
