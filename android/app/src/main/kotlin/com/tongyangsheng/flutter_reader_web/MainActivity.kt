77c8e5a7067d0a535b0b8908ebe070f8204fe8b4
“tongyangsheng” <964997115@qq.com>
2019-11-01 16:08:24 +0800

    项目t开通Flutter Web端

---
+++ android/app/src/main/kotlin/com/tongyangsheng/flutter_reader_web/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tongyangsheng.flutter_reader_web
+
+import android.os.Bundle
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
