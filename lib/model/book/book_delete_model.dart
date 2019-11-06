09d5cae5e1f08287e70814908bb91fc3256763f9
“tongyangsheng” <964997115@qq.com>
2019-11-06 17:12:23 +0800

    删除书架书籍Model

---
+++ lib/model/book/book_delete_model.dart
@@ -0,0 +1,26 @@
+class BookDeleteModel {
+  bool _success;
+  String _data;
+
+  BookDeleteModel({bool success, String data}) {
+    this._success = success;
+    this._data = data;
+  }
+
+  bool get success => _success;
+  set success(bool success) => _success = success;
+  String get data => _data;
+  set data(String data) => _data = data;
+
+  BookDeleteModel.fromJson(Map<String, dynamic> json) {
+    _success = json['success'];
+    _data = json['data'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['success'] = this._success;
+    data['data'] = this._data;
+    return data;
+  }
+}
