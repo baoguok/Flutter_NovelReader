d6bc775de433ec7578f1a31e46623137cb79a8c7
“tongyangsheng” <964997115@qq.com>
2019-11-07 16:40:47 +0800

    浏览历史接口新增

--- lib/dao/book_collect_data_manager.dart
+++ lib/dao/book_collect_data_manager.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:math';
 import 'package:flutter_reader/model/book/book_collect_model.dart';
 import 'package:flutter_reader/model/book/book_delete_model.dart';
+import 'package:flutter_reader/model/book/book_history_model.dart';
 import 'package:flutter_reader/model/book/bookshelf_model.dart';
 import 'package:http/http.dart' as http;
 
@@ -23,6 +24,7 @@ class BookCollectDao{
       throw Exception('加载bookcollect接口失败');
     }
   }
+
   static Future<BookShelfModel> fetchBookShelf() async{
     final response = await http.get(BaseURL+'/record/favorite?format=normal',
         headers: {'BSAuthorization':BSAuthorization,'READING':'API'});
@@ -37,6 +39,7 @@ class BookCollectDao{
       throw Exception('加载bookshelf接口失败');
     }
   }
+
   static Future<BookDeleteModel> deleteBook(List<String> list) async{
     print(list);
     String idlist = '';
@@ -49,7 +52,6 @@ class BookCollectDao{
           idlist = idlist + ',' + '${list[i]}';
         }
     }
-    print(BaseURL+'manage/favorite?id=${idlist}');
     final response = await http.delete(BaseURL+'/manage/favorite?id=${idlist}',
         headers: {'BSAuthorization':BSAuthorization,'READING':'API'});
     if( response.statusCode == 200){
@@ -61,6 +63,21 @@ class BookCollectDao{
     else
     {
       throw Exception('加载bookdelete接口失败');
+    }
+  }
+
+  static Future<BookHistoryModel> fetchBookHistory() async {
+    final response = await http.get(BaseURL+'/record/footprint',
+        headers: {'BSAuthorization':BSAuthorization,'READING':'API'});
+    if( response.statusCode == 200){
+      //解决中文乱码
+      Utf8Decoder utf8decoder = Utf8Decoder();
+      final json = jsonDecode(utf8decoder.convert(response.bodyBytes));
+      return BookHistoryModel.fromJson(json);
+    }
+    else
+    {
+      throw Exception('加载bookhistory接口失败');
     }
   }
 }
