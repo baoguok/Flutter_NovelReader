431e2ddbb2f7edb8a0ddce26f5bf1c89dd96e1c2
“tongyangsheng” <964997115@qq.com>
2019-11-07 16:41:37 +0800

    新增接口当前章节数据

--- lib/model/book/book_cata_info_model.dart
+++ lib/model/book/book_cata_info_model.dart
@@ -28,16 +28,20 @@ class CataInfoModel {
 }
 
 class CataInfoData {
+  Current _current;
   First _first;
   Last _last;
   int _total;
 
-  CataInfoData({First first, Last last, int total}) {
+  CataInfoData({Current current, First first, Last last, int total}) {
+    this._current = current;
     this._first = first;
     this._last = last;
     this._total = total;
   }
 
+  Current get current => _current;
+  set current(Current current) => _current = current;
   First get first => _first;
   set first(First first) => _first = first;
   Last get last => _last;
@@ -46,6 +50,8 @@ class CataInfoData {
   set total(int total) => _total = total;
 
   CataInfoData.fromJson(Map<String, dynamic> json) {
+    _current =
+    json['current'] != null ? new Current.fromJson(json['current']) : null;
     _first = json['first'] != null ? new First.fromJson(json['first']) : null;
     _last = json['last'] != null ? new Last.fromJson(json['last']) : null;
     _total = json['total'];
@@ -53,6 +59,9 @@ class CataInfoData {
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this._current != null) {
+      data['current'] = this._current.toJson();
+    }
     if (this._first != null) {
       data['first'] = this._first.toJson();
     }
@@ -60,6 +69,39 @@ class CataInfoData {
       data['last'] = this._last.toJson();
     }
     data['total'] = this._total;
+    return data;
+  }
+}
+
+class Current {
+  String _id;
+  String _name;
+  String _logo;
+
+  Current({String id, String name, String logo}) {
+    this._id = id;
+    this._name = name;
+    this._logo = logo;
+  }
+
+  String get id => _id;
+  set id(String id) => _id = id;
+  String get name => _name;
+  set name(String name) => _name = name;
+  String get logo => _logo;
+  set logo(String logo) => _logo = logo;
+
+  Current.fromJson(Map<String, dynamic> json) {
+    _id = json['id'];
+    _name = json['name'];
+    _logo = json['logo'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this._id;
+    data['name'] = this._name;
+    data['logo'] = this._logo;
     return data;
   }
 }
