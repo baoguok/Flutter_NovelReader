bd133471c7e5bbfd2ae0ba24af5c7f957b138253
“tongyangsheng” <964997115@qq.com>
2019-11-19 10:24:22 +0800

    加载器处理

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_reader/navigator/tab_navigator.dart';
 import 'package:flutter_reader/tools/global_state.dart';
+import 'package:overlay_support/overlay_support.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -20,18 +21,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: '爱看',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-        accentColor: Color(0xffe53935),
-        buttonTheme: ButtonThemeData(
-          buttonColor: Color(0xffe53935),
-          shape: RoundedRectangleBorder(),
-        )
+    return OverlaySupport(
+      child: MaterialApp(
+        title: '爱看',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            primarySwatch: Colors.red,
+            accentColor: Color(0xffe53935),
+            buttonTheme: ButtonThemeData(
+              buttonColor: Color(0xffe53935),
+              shape: RoundedRectangleBorder(),
+            )
+        ),
+        home: TabNavigator(),
       ),
-      home: TabNavigator(),
     );
   }
 }
